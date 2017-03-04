2dfde98f3a03b78757fd9c33d76e33cf05298a54
Randall Porter <porterrandall@yahoo.com>
2017-03-04 13:28:22 -0800

    added main, pulled changes

---
+++ app/src/main/java/randall/porter/com/groupcalendar/MainActivity.java
@@ -0,0 +1,13 @@
+package randall.porter.com.groupcalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
