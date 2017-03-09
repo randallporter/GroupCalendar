035766501cfb80314f33ce00fabbeb0e679ce929
Randall Porter <porterrandall@yahoo.com>
2017-03-08 18:52:38 -0800

    Have  insert into calendar and retrieve from calendar proof of concept activities done. Still need to move their functionality off of an activity and into java class.

--- app/src/main/java/randall/porter/com/groupcalendar/MainActivity.java
+++ app/src/main/java/randall/porter/com/groupcalendar/MainActivity.java
@@ -1,13 +1,45 @@
 package randall.porter.com.groupcalendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        LinearLayout activityLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        activityLayout.setLayoutParams(lp);
+        activityLayout.setOrientation(LinearLayout.VERTICAL);
+        activityLayout.setPadding(16, 16, 16, 16);
+
+        ViewGroup.LayoutParams tlp = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+
+        final Button apiButton;
+        apiButton = new Button(this);
+        apiButton.setText("API");
+        apiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), calendarInsertTest.class);
+                startActivity(intent);
+            }
+        });
+        activityLayout.addView(apiButton);
+        setContentView(activityLayout);
+
     }
 }
