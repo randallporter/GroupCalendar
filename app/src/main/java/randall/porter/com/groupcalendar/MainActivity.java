46cc55c6952f13f003dfa5acca7608d1c25a98f0
Sasha Iliyn <alexanramiliyn@gmail.com>
2017-03-15 17:58:51 -0700

    Adding styling

--- app/src/main/java/randall/porter/com/groupcalendar/MainActivity.java
+++ app/src/main/java/randall/porter/com/groupcalendar/MainActivity.java
@@ -18,65 +18,67 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, NavDrawer.class);
+        startActivity(intent);
 
-        LinearLayout activityLayout = new LinearLayout(this);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT);
-        activityLayout.setLayoutParams(lp);
-        activityLayout.setOrientation(LinearLayout.VERTICAL);
-        activityLayout.setPadding(16, 16, 16, 16);
-
-
-        final Button apiButton;
-        apiButton = new Button(this);
-        apiButton.setText("Sync Account");
-        apiButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), AccountActivity.class);
-                startActivity(intent);
-            }
-        });
-        activityLayout.addView(apiButton);
-
-        final Button ViewEvents;
-        ViewEvents = new Button(this);
-        ViewEvents.setText("View Events");
-        ViewEvents.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), CalendarEventList.class);
-                startActivity(intent);
-            }
-        });
-        activityLayout.addView(ViewEvents);
-
-        final Button addEvent;
-        addEvent = new Button(this);
-        addEvent.setText("Add Event");
-        addEvent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), AddEvent.class);
-                startActivity(intent);
-            }
-        });
-        activityLayout.addView(addEvent);
-
-        final Button addCalendar;
-        addCalendar = new Button(this);
-        addCalendar.setText("Add Calendar");
-        addCalendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), ManageCalendarList.class);
-                startActivity(intent);
-            }
-        });
-        activityLayout.addView(addCalendar);
-
-        setContentView(activityLayout);
+//        LinearLayout activityLayout = new LinearLayout(this);
+//        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.MATCH_PARENT);
+//        activityLayout.setLayoutParams(lp);
+//        activityLayout.setOrientation(LinearLayout.VERTICAL);
+//        activityLayout.setPadding(16, 16, 16, 16);
+//
+//
+//        final Button apiButton;
+//        apiButton = new Button(this);
+//        apiButton.setText("Sync Account");
+//        apiButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(), AccountActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        activityLayout.addView(apiButton);
+//
+//        final Button ViewEvents;
+//        ViewEvents = new Button(this);
+//        ViewEvents.setText("View Events");
+//        ViewEvents.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(), CalendarEventList.class);
+//                startActivity(intent);
+//            }
+//        });
+//        activityLayout.addView(ViewEvents);
+//
+//        final Button addEvent;
+//        addEvent = new Button(this);
+//        addEvent.setText("Add Event");
+//        addEvent.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(), AddEvent.class);
+//                startActivity(intent);
+//            }
+//        });
+//        activityLayout.addView(addEvent);
+//
+//        final Button addCalendar;
+//        addCalendar = new Button(this);
+//        addCalendar.setText("Add Calendar");
+//        addCalendar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(v.getContext(), ManageCalendarList.class);
+//                startActivity(intent);
+//            }
+//        });
+//        activityLayout.addView(addCalendar);
+//
+//        setContentView(activityLayout);
 
 
     }
--- app/src/main/java/randall/porter/com/groupcalendar/NavDrawer.java
+++ app/src/main/java/randall/porter/com/groupcalendar/NavDrawer.java
@@ -1,5 +1,6 @@
 package randall.porter.com.groupcalendar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class NavDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,14 +26,14 @@ public class NavDrawer extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,18 +83,19 @@ public class NavDrawer extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_account) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            Intent intent = new Intent(NavDrawer.this, AccountActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_events) {
+            Intent intent = new Intent(NavDrawer.this, CalendarEventList.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_add) {
+            Intent intent = new Intent(NavDrawer.this, AddEvent.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_calendar) {
+            Intent intent = new Intent(NavDrawer.this, ManageCalendarList.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
